ed05ab96f4631024d02c75e2d508389b2942d059
Tyler Gaugler <tylergaugler16@gmail.com>
2019-02-11 20:35:56 -0500

    Starting to add auth

--- backend/app/controllers/authentications_controller.rb
+++ backend/app/controllers/authentications_controller.rb
@@ -1,10 +1,12 @@
+# Handling authenticated users
 class AuthenticationsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: :handle_authenticated_user
+  skip_before_action :verify_authenticity_token,
+                     only: :handle_authenticated_user
   def handle_authenticated_user
     # @user = User.find_or_create_from_auth_hash(auth_hash)
     # self.current_user = @user
     puts "yoooo #{auth_hash.inspect}"
-    redirect_to '/'
+    redirect_to 'http://localhost:3000?auth_token=123344'
   end
 
   protected
