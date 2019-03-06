c8d729ef0a916858a5adecfc86ab7154b501bda4
Tyler Gaugler <tylergaugler16@gmail.com>
2019-03-06 15:20:38 -0500

    Prevent buttons from overlapping

--- frontend/src/containers/landing_pages/_SignupAndLoginInactiveContainer.tsx
+++ frontend/src/containers/landing_pages/_SignupAndLoginInactiveContainer.tsx
@@ -1,36 +1,54 @@
 import * as React from "react";
 
-
 interface IProps {
   changeView: (newValue: "signup" | "login") => void;
   currentView: "signup" | "login";
 }
 
-
-class SignupAndLoginInactiveContainer extends React.Component<IProps,{}> {
+class SignupAndLoginInactiveContainer extends React.Component<IProps, {}> {
   public constructor(props: IProps) {
     super(props);
   }
 
   public render() {
-    const {currentView, changeView} = this.props;
+    const { currentView, changeView } = this.props;
+
     return (
-
       <div className="signup-and-login-inactive-container">
+        <div
+          className={`login-view-content ${
+            currentView === "login" ? "should-show" : "should-hide"
+          }`}
+        >
+          <h1 className="title-1 white-font">Welcome!</h1>
+          <p className="white-font">
+            If you dont have an account{" "}
+            <span onClick={() => changeView("signup")}>create an account.</span>
+          </p>
+            <div className="button-container">
+              <button className="button large-button white-transparent-button" onClick={() => changeView("signup")}>
+                <span>Sign Up</span>
+              </button>
+            </div>
 
-          <div className={`login-view-content ${currentView === "login"? "should-show" : "should-hide"}`}>
-            <h1 className="title-1 white-font">Welcome!</h1>
-            <p className="white-font">If you dont have an account <span onClick={() => changeView("signup")}>create an account.</span></p>
-            <button className="button large-button white-transparent-button" onClick={() => changeView("signup")}><span>Sign Up</span></button>
+        </div>
+        <div
+          className={`signup-view-content ${
+            currentView === "signup" ? "should-show" : "should-hide"
+          }`}
+        >
+          <h1 className="title-1 white-font">Good to see you again!</h1>
+          <p className="white-font">
+            If you already have an account,login{" "}
+            <span onClick={() => changeView("login")}>here.</span>
+          </p>
+          <div className="button-container">
+            <button className="button large-button white-transparent-button" onClick={() => changeView("login")}>
+              <span>Log In</span>
+            </button>
           </div>
-          <div className={`signup-view-content ${currentView === "signup"? "should-show" : "should-hide"}`}>
-            <h1 className="title-1 white-font">Good to see you again!</h1>
-            <p className="white-font">If you already have an account,login <span onClick={() => changeView("login")}>here.</span></p>
-            <button className="button large-button white-transparent-button" onClick={() => changeView("login")}><span>Log In</span></button>
-          </div>
-
+        </div>
       </div>
-
     );
   }
 }
