d3e8e528575c94d51eec24d02589476c02d105d5
Tyler Gaugler <tylergaugler16@gmail.com>
2019-04-27 19:12:37 -0400

    Add production domain to cors policy

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -17,7 +17,7 @@ module Myapp
     # the framework and any gems in your application.
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins ['http://localhost:3000', 'https://widgetly-app.herokuapp.com']
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
