557687b7234d8c9e5204b90b9337b5630b38ea7a
Tyler Gaugler <tylergaugler16@gmail.com>
2019-05-14 21:09:42 -0400

    Fix migration

--- backend/db/migrate/20190513033042_remove_fields_from_audio_datum.rb
+++ backend/db/migrate/20190513033042_remove_fields_from_audio_datum.rb
@@ -1,7 +1,5 @@
 class RemoveFieldsFromAudioDatum < ActiveRecord::Migration[5.2]
   def change
-    remove_index :audio_data, :file_upload_id
-    remove_column :audio_data, :file_upload_id
     remove_column :audio_data, :file_size
     remove_column :audio_data, :file_name
     remove_column :audio_data, :s3_object_url
