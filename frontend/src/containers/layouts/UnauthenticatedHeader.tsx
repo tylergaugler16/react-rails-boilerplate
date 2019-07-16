7b6de195a13fe49ddbbd78bc6aa0c22cc5e78fb4
Tyler Gaugler <tylergaugler16@gmail.com>
2019-07-16 03:40:12 -0400

    Update copy and style of unathenticated header

--- frontend/src/containers/layouts/UnauthenticatedHeader.tsx
+++ frontend/src/containers/layouts/UnauthenticatedHeader.tsx
@@ -13,15 +13,31 @@ class UnauthenticatedHeader extends React.Component<IProps, {}> {
     return (
       <div className="columns is-gapless is-vcentered is-full-height header-content">
         <div className="column">Widgetly</div>
-        <div className="column">
-          <Link to="/users/login">Login</Link>
+        <div className="column is-narrow is-pulled-right login-link">
+
         </div>
-        <div className="column">
-          <Link to="/users/signup">Signup</Link>
+        <div className="column is-narrow is-pulled-right">
+
         </div>
-        <div className="column">
-          <Link to="/">Home</Link>
+
+
+        <div className="column is-narrow is-pulled-right header-links-container is-small">
+          <div className="columns is-gapless is-vcentered">
+
+            <div className="column has-text-centered">
+              <Link to="/users/login">Login</Link>
+            </div>
+
+            <div className="column is-narrow link-break" />
+
+            <div className="column has-text-centered">
+              <Link to="/users/signup">Signup</Link>
+            </div>
+
+          </div>
         </div>
+
+
       </div>
     );
   }
